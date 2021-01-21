9275c53dc64e84db712143c812928974f9e5f78d
K-Mano <39230424+K-Mano@users.noreply.github.com>
2021-01-21 09:20:03 +0900

    Main/AddActivity fix

--- app/src/main/java/org/ravenest/octify/AddActivity.java
+++ app/src/main/java/org/ravenest/octify/AddActivity.java
@@ -1,22 +1,89 @@
 package org.ravenest.octify;
 
+import android.app.SearchManager;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.core.util.Pair;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
-import android.os.Bundle;
+import com.google.android.material.datepicker.MaterialDatePicker;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.timepicker.MaterialTimePicker;
+import com.google.android.material.timepicker.TimeFormat;
+
+import java.util.Calendar;
+
+import static androidx.fragment.app.DialogFragment.STYLE_NO_FRAME;
 
 public class AddActivity extends AppCompatActivity {
+
+    FloatingActionButton submit;
+    ConstraintLayout date_set;
+    ConstraintLayout time_set;
+
+    FragmentManager fm;
+    FragmentTransaction ft;
+
+    MaterialDatePicker.Builder<Pair<Long, Long>> builder = MaterialDatePicker.Builder.dateRangePicker();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        Calendar calendar = Calendar.getInstance();
+
+        int year = calendar.get(Calendar.YEAR);
+
         setContentView(R.layout.activity_add);
         Toolbar toolbar = findViewById(R.id.toolbar_aa);
 
+        // ToolBarをActionBarとして設定
         setSupportActionBar(toolbar);
 
+        // ActionBarの設定
         getSupportActionBar().setDisplayShowHomeEnabled(true);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        // コントロールの取得
+        submit = findViewById(R.id.submit_button);
+        date_set = findViewById(R.id.date_set);
+        time_set = findViewById(R.id.time_set);
+
+        fm = getSupportFragmentManager();
+
+        submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        date_set.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                MaterialDatePicker bd = MaterialDatePicker.Builder.datePicker().build();
+                bd.addOnCancelListener(new DialogInterface.OnCancelListener() {
+                    @Override
+                    public void onCancel(DialogInterface dialog) {
+
+                    }
+                });
+                bd.show(fm,"DatePicker");
+            }
+        });
+        time_set.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                MaterialTimePicker bt = new MaterialTimePicker.Builder().setTimeFormat(TimeFormat.CLOCK_12H).build();
+                bt.show(fm,"TimePicker");
+            }
+        });
     }
 
     @Override
--- app/src/main/java/org/ravenest/octify/MainActivity.java
+++ app/src/main/java/org/ravenest/octify/MainActivity.java
@@ -2,14 +2,39 @@ package org.ravenest.octify;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.cardview.widget.CardView;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.res.Resources;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.google.android.material.bottomsheet.BottomSheetBehavior;
+
+import java.net.URL;
+
+import jp.wasabeef.blurry.Blurry;
+
+import static java.security.AccessController.getContext;
 
 public class MainActivity extends AppCompatActivity {
+
+    private BottomSheetBehavior sheetBehavior;
+    private CardView bottom_sheet;
+    private TextView sheet_state;
+
+    private ConstraintLayout rootView;
+    private ImageView blur;
+
+    private Resources r;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,8 +42,59 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.main_toolbar);
 
+        rootView = findViewById(R.id.base);
+
         setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("");
+
+        bottom_sheet = findViewById(R.id.sheet_base);
+        sheetBehavior = BottomSheetBehavior.from(bottom_sheet);
+        sheet_state = findViewById(R.id.text_sheet_state);
+
+        r = getResources();
+
+        blur=findViewById(R.id.imageView);
+
+        sheetBehavior.addBottomSheetCallback(new BottomSheetBehavior.BottomSheetCallback() {
+            @Override
+            public void onStateChanged(View view, int newState) {
+                boolean flag=false;
+
+                switch (newState) {
+                    case BottomSheetBehavior.STATE_HIDDEN:
+                        break;
+                    case BottomSheetBehavior.STATE_EXPANDED:
+                        sheet_state.setText("今日の予定を隠す");
+                        break;
+                    case BottomSheetBehavior.STATE_COLLAPSED:
+                        sheet_state.setText("今日の予定を表示");
+                        Blurry.delete(rootView);
+                        flag=false;
+                        break;
+                    case BottomSheetBehavior.STATE_DRAGGING:
+                        if(!flag){
+                            flag=true;
+                            Blurry.with(getApplicationContext()).async().radius(25).capture(rootView).into(blur);
+                        }
+                        break;
+                    case BottomSheetBehavior.STATE_SETTLING:
+                        break;
+                }
+            }
+
+            @Override
+            public void onSlide(View view, float offset) {
+                blur.setAlpha(offset);
+                //bottom_sheet.setAlpha(1-(offset*0.05f));
+                if(offset==0){
+                    blur.setVisibility(View.GONE);
+                }else{
+                    if(blur.getVisibility()==View.GONE)blur.setVisibility(View.VISIBLE);
+                }
+            }
+        });
     }
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.main_menu, menu);
